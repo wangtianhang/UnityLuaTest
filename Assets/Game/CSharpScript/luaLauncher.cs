c46fc84b0819988abc87391353c97d3f73552328
wangtianhang <690879430@qq.com>
2017-08-22 18:01:37 +0800

    测试lua框架

--- Assets/Game/CSharpScript/luaLauncher.cs
+++ Assets/Game/CSharpScript/luaLauncher.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using LuaInterface;
+
 public class luaLauncher : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+	    // todo 从远程服务器拉取lua框架
+
+        // 启动lua框架
+        StartCoroutine(LaunchLua());
 	}
+
+    IEnumerator LaunchLua()
+    {
+        string url = "file://" + Application.streamingAssetsPath + "/FrameLuaScript/UpdateRes.lua";
+        WWW www = new WWW(url);
+        yield return www;
+        string luaText = www.text;
+
+        LuaState lua = new LuaState();
+        lua.Start();
+        lua.DoString(luaText);
+        //lua.CheckTop();
+        //lua.Dispose();
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
 }
